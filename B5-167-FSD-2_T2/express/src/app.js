b81144632998dcdaaa4e895bb5330059dc6c928c
Devam shah <116349183+DevamShah1211@users.noreply.github.com>
2025-05-09 09:00:50 +0530

    Delete B5-167-FSD-2_T2/express directory

--- B5-167-FSD-2_T2/express/src/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static("../public/html/"))
-app.use(express.static("../public/css/"))
-app.use(express.static("../public/image/"))
-app.listen(8081, () => { console.log("sever is listen http://localhost:8081") })
--- B5-167-FSD-2_T2/express/src/image.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static("../public",{index:"form.html"}))
-app.get("/process",(req,res)=>{
-    data={user:req.query.name,pass:req.query.pass}
-    console.log(data)
-    res.send(data)
-})
-app.listen(8081, () => { console.log("sever is listen http://localhost:8081") })
