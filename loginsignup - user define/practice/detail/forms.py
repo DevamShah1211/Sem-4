eaaa3094593906affac03e67aab0722488f2e757
Devam shah <116349183+DevamShah1211@users.noreply.github.com>
2025-07-16 13:16:23 +0530

    Delete loginsignup - user define directory

--- loginsignup - user define/practice/detail/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-# from loginsignup.models import AddGame1
-from detail.models import Student
-class StudForm(forms.ModelForm):
-    class Meta:
-        model=Student
-        fields=["name","age"]
